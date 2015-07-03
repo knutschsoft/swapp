1f8a3860a7ecd0b2d762021f11adf7fb37c56403
robertfausk <robertfreigang@gmx.de>
2015-07-03 07:16:47 +0200

    init sonata admin bundle
    * use doctrine orm admin bundle
    * use basic configuration
    * access via /admin respective /admin/dashboard

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,13 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+            // SonataAdminBundle
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
