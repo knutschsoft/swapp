1e972239a44f7cb314e6a0e2e05f0c3537274950
robertfausk <dbsaga0815>
2015-07-28 21:27:47 +0200

    add UserParamConverter to get currently logged in user in all controllers

---
+++ src/AppBundle/ParamConverter/UserParamConverter.php
@@ -0,0 +1,50 @@
+<?php
+namespace AppBundle\ParamConverter;
+
+use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class UserParamConverter implements ParamConverterInterface
+{
+    private $tokenStorage;
+
+    /**
+     * @param TokenStorageInterface $tokenStorage
+     */
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+    /**
+     * Stores the object in the request.
+     *
+     * @param Request        $request       The request
+     * @param ParamConverter $configuration Contains the name, class and options of the object
+     *
+     * @return bool True if the object has been successfully set, else false
+     */
+    public function apply(Request $request, ParamConverter $configuration)
+    {
+        $user = $this->tokenStorage->getToken()->getUser();
+        $param = $configuration->getName();
+        $request->attributes->set($param, $user);
+
+        return true;
+    }
+    /**
+     * Checks if the object is supported.
+     *
+     * @param ParamConverter $configuration
+     *
+     * @return bool True if the object is supported, else false
+     */
+    public function supports(ParamConverter $configuration)
+    {
+
+        return $configuration->getClass() === User::class;
+    }
+}
