f9d751f70e824938c3af0b065063bbeeaad7abe0
robertfausk <dbsaga0815>
2015-07-29 08:05:12 +0200

    fix and improve tests
    * test user log in for frontend and backend
    * add availability tests:
    simply call url with logged in user and check status code
    * use naming EdgeToEdge in favor of Integration or Controller
    
    * define default redirect route after login; it's walk_homeScreen

---
+++ src/AppBundle/Tests/EdgeToEdge/ApplicationAvailabilityTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace AppBundle\Tests\EdgeToEdge;
+
+class ApplicationAvailabilityFunctionalTest extends BaseWebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $this->logIn();
+        $this->client->request('GET', $url);
+
+        $this->assertTrue(
+            $this->client->getResponse()->isSuccessful(),
+            'status code is ' . $this->client->getResponse()->getStatusCode()
+        );
+    }
+
+    public function urlProvider()
+    {
+        return array(
+            array('/createwalk'),
+            array('/walks'),
+            array('/tag'),
+            array('/eadmin/?action=list&entity=Team'),
+            array('/eadmin/?action=list&entity=Walk'),
+            array('/eadmin/?action=list&entity=WayPoint'),
+            array('/eadmin/?action=list&entity=Tag'),
+            array('/eadmin/?action=list&entity=User'),
+            array('/eadmin/?action=list&entity=Guest'),
+            array('/eadmin/?action=list&entity=SystemicQuestion'),
+            // ...
+        );
+    }
+}
---
+++ src/AppBundle/Tests/EdgeToEdge/UserLoginTest.php
@@ -0,0 +1,66 @@
+<?php
+namespace AppBundle\Tests\EdgeToEdge;
+
+class UserLoginTest extends BaseWebTestCase
+{
+    public function testUserLoginBackendFailsWithWrongRole()
+    {
+        $this->client->followRedirects(true);
+        $crawler = $this->client->request('GET', '/eadmin');
+
+        $form = $crawler->selectButton('Anmelden')->form(array(
+            '_username' => self::USERNAME,
+            '_password' => self::PASSWORD,
+            '_remember_me' => 'on',
+        ));
+
+        $crawler = $this->client->submit($form);
+
+        $this->assertEquals(
+            403,
+            $this->client->getResponse()->getStatusCode(),
+            'Response have wrong status code'
+        );
+//        $this->assertTrue($crawler->selectButton('Anmelden')->count() === 1, 'Button "Anmelden" not found');
+    }
+
+    public function testUserLoginBackend()
+    {
+        $this->user->setRoles(array('ROLE_SUPER_ADMIN', 'ROLE_ADMIN', 'ROLE_USER'));
+        $this->user->setSuperAdmin(true);
+        $this->userManager->updateUser($this->user);
+
+        $this->userManager->refreshUser($this->user);
+
+        $this->client->followRedirects(true);
+        $crawler = $this->client->request('GET', '/eadmin');
+
+        $form = $crawler->selectButton('Anmelden')->form(array(
+            '_username' => self::USERNAME,
+            '_password' => self::PASSWORD,
+            '_remember_me' => 'on',
+        ));
+
+        $crawler = $this->client->submit($form);
+
+        $this->assertTrue($this->client->getResponse()->isSuccessful(), 'Response not successful');
+        $this->assertTrue($crawler->selectButton('Anmelden')->count() === 0, 'Button "Anmelden" found');
+    }
+
+    public function testUserLoginFrontend()
+    {
+        $this->client->followRedirects(true);
+        $crawler = $this->client->request('GET', '/login');
+
+        $form = $crawler->selectButton('Anmelden')->form(array(
+            '_username' => self::USERNAME,
+            '_password' => self::PASSWORD,
+            '_remember_me' => 'on',
+        ));
+
+        $crawler = $this->client->submit($form);
+
+        $this->assertTrue($this->client->getResponse()->isSuccessful(), 'Response not successful');
+        $this->assertTrue($crawler->filter('Anmelden')->count() === 0, 'Button "Anmelden" found');
+    }
+}
