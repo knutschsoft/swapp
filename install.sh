b1385fbba1adaac75d0ea3adc7d39366ab0acde6
robertfausk <dbsaga0815>
2015-07-29 08:22:39 +0200

    add basic install and deploy script and update composer phar

---
+++ install.sh
@@ -0,0 +1,16 @@
+#curl -sS https://getcomposer.org/installer | php
+#php composer.phar install
+
+php app/console doctrine:database:drop --if-exists --force
+php app/console doctrine:database:create --if-not-exists
+
+#php app/console doctrine:migrations:migrate --no-interaction
+#php app/console doctrine:schema:update --force --no-interaction
+php app/console doctrine:schema:create --no-interaction
+
+php app/console doctrine:fixtures:load --no-interaction
+
+#npm install
+#./node_modules/.bin/bower install
+
+#./bin/phpunit -c app
