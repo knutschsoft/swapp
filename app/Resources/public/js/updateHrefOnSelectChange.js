bf7530f61a3de081ad4ff5a68c0475dd025d0fa2
robertfausk <robertfreigang@gmx.de>
2015-09-08 09:50:26 +0200

    prevent js error when element is not available

--- app/Resources/public/js/updateHrefOnSelectChange.js
+++ app/Resources/public/js/updateHrefOnSelectChange.js
@@ -1,5 +1,12 @@
 module.exports = function(selectClass, hrefClass) {
-    window.document.getElementsByClassName(selectClass)[0].addEventListener('change', function () {
+    var selectElement = window.document.getElementsByClassName(selectClass)[0];
+
+    if (selectElement === undefined) {
+
+        return;
+    }
+
+    selectElement.addEventListener('change', function () {
         var href, hrefParts, linkElem;
         linkElem = window.document.getElementsByClassName(hrefClass)[0];
         href = linkElem.getAttribute('href');
