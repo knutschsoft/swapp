382239fea044555a54a165e0757f74f3547f1154
robertfausk <robert.freigang@gmx.de>
2015-10-23 22:13:27 +0200

    add elao/web-profiler-extra-bundle
    * needed to readdsymfony/assetic-bundle at least as dev requirement to get web-profiler-extra-bundle work properly
    ** issue created at https://github.com/Elao/WebProfilerExtraBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,8 +26,10 @@ class AppKernel extends Kernel
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\AsseticBundle\AsseticBundle();
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Elao\WebProfilerExtraBundle\WebProfilerExtraBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
