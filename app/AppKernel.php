df623942fe5188aa42e42c0827d3be35d2883174
robertfausk <robertfreigang@gmx.de>
2016-01-27 15:29:36 +0700

    remove assetic bundle and composer update #1

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,7 +27,6 @@ class AppKernel extends Kernel
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Symfony\Bundle\AsseticBundle\AsseticBundle();
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Elao\WebProfilerExtraBundle\WebProfilerExtraBundle();
