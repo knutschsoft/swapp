18123077f68b89dab2d4c99a171c5e6ec3ecf860
robertfausk <robertfreigang@gmx.de>
2016-12-09 17:31:21 +0100

    add heroku specific db environment settings

---
+++ src/AppBundle/Composer/HerokuEnvironment.php
@@ -0,0 +1,31 @@
+<?php
+namespace AppBundle\Composer;
+
+use Composer\Script\Event;
+
+class HerokuEnvironment
+{
+    /**
+     * Populate Heroku environment
+     *
+     * @param Event $event Event
+     */
+    public static function populateEnvironment(Event $event)
+    {
+        $url = getenv('CLEARDB_DATABASE_URL'); // If MySQL is chosen
+        // $url = getenv('HEROKU_POSTGRESQL_IVORY_URL'); If PostgreSQL is chosen
+
+        if ($url) {
+            $url = parse_url($url);
+            putenv("SYMFONY__DATABASE_HOST={$url['host']}");
+            putenv("SYMFONY__DATABASE_USER={$url['user']}");
+            putenv("SYMFONY__DATABASE_PASSWORD={$url['pass']}");
+
+            $db = substr($url['path'], 1);
+            putenv("SYMFONY__DATABASE_NAME={$db}");
+        }
+
+        $io = $event->getIO();
+        $io->write('CLEARDB_DATABASE_URL=' . getenv('CLEARDB_DATABASE_URL'));
+    }
+}
