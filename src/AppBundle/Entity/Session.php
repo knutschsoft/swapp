93cbe14b626237ec6feb6c7ece632e9156143d72
pueppiblue <nils.freigang@projektmotor.de>
2018-06-08 22:44:40 +0200

    save session in database #73
      * since sessions created on filesystem are set by docker user
        restarting the container would result in invalid sessions that cant
        be removed because of file permissions
        Also when leveraging a load balancer there would be several file-
        system instances. One for each app instance, but only one session
        table in the database. Thus saving session data in database is
        implemented using the PdoSessionHandler
      * Swapp users requested longer session lifetime because they had to
        repeatedly sign in again which proved to be a nuisance while being
        on their patrol. Session lifetime is increased to 1 year.

---
+++ src/AppBundle/Entity/Session.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\Types\Resource;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="sessions")
+ */
+class Session
+{
+
+    /**
+     * @ORM\Id()
+     * @ORM\Column(name="sess_id",type="string", length=128, nullable=false)
+     * @var string
+     */
+    protected $sessId;
+
+    /**
+     * @ORM\Column(name="sess_data",type="blob", nullable=false)
+     * @var Resource
+     */
+    protected $sessData;
+
+    /**
+     * @ORM\Column(name="sess_time",type="integer", nullable=false, options={"unsigned": false})
+     * @var int
+     */
+    protected $sessTime;
+
+    /**
+     * @ORM\Column(name="sess_lifetime",type="integer", nullable=false)
+     * @var int
+     */
+    protected $sessLifetime;
+
+    public function getSessId()
+    {
+        return $this->sessId;
+    }
+
+    public function setSessId($sessId)
+    {
+        $this->sessId = $sessId;
+    }
+
+    public function getSessData()
+    {
+        return $this->sessData;
+    }
+
+    public function setSessData(Resource $sessData)
+    {
+        $this->sessData = $sessData;
+    }
+
+    public function getSessTime()
+    {
+        return $this->sessTime;
+    }
+
+    public function setSessTime($sessTime)
+    {
+        $this->sessTime = $sessTime;
+    }
+
+    public function getSessLifetime()
+    {
+        return $this->sessLifetime;
+    }
+
+    public function setSessLifetime($sessLifetime)
+    {
+        $this->sessLifetime = $sessLifetime;
+    }
+}
