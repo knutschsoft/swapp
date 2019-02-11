5f2bc05d58feba79af33bd2ede1859d5e41c9dcd
robertfausk <robertfreigang@gmx.de>
2019-02-11 10:26:05 +0100

    Install and use phpstan for travis #92
    * introduce timestampable on WayPoint cause image handling requires it
    add missing migration file

---
+++ app/DoctrineMigrations/Version20190211091823.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190211091823 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql(
+            'ALTER TABLE way_point ADD createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, ADD updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP'
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql(
+            'ALTER TABLE way_point DROP createdAt, DROP updatedAt'
+        );
+    }
+}
