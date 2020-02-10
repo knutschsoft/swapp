8f0057d195de603852decbf6ae9981316d0f451f
Robert Freigang <robertfreigang@gmx.de>
2020-02-10 13:44:12 +0100

    Apply fixes from StyleCI (#130)
    [ci skip] [skip ci]

--- src/Migrations/Version20200209215126.php
+++ src/Migrations/Version20200209215126.php
@@ -12,15 +12,15 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20200209215126 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql(
             'UPDATE `walk` SET `ageRanges` = REPLACE(`ageRanges`, "AppBundle", "App");'
@@ -36,7 +36,7 @@ final class Version20200209215126 extends AbstractMigration
         );
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
     }
 }
