bb42e8d0681f1f0fea5429a9d2a9cbbcd53d1cb7
robertfausk <robertfreigang@gmx.de>
2021-05-07 14:20:31 +0200

    Add openapi factory and customize api docs page #168
    https://api-platform.com/docs/core/openapi/

---
+++ src/OpenApi/OpenApiFactory.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\OpenApi;
+
+use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\Core\OpenApi\Model;
+use ApiPlatform\Core\OpenApi\Model\Info;
+use ApiPlatform\Core\OpenApi\OpenApi;
+use Webmozart\Assert\Assert;
+
+class OpenApiFactory implements OpenApiFactoryInterface
+{
+    private OpenApiFactoryInterface $decorated;
+
+    public function __construct(OpenApiFactoryInterface $decorated)
+    {
+        $this->decorated = $decorated;
+    }
+
+    public function __invoke(array $context = []): OpenApi
+    {
+        $openApi = $this->decorated->__invoke($context);
+
+        $info = new Model\Info(
+            'Swapp',
+            'v1',
+            'Description of streetworkapp API'
+        );
+        Assert::isInstanceOf($info, Info::class);
+
+        return $openApi->withInfo($info);
+    }
+}
