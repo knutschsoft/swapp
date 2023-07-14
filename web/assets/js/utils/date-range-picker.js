98b34aca9239e0b7277288a9ff42ba21daac1fcc
robertfausk <robertfreigang@gmx.de>
2023-07-14 09:19:21 +0200

    Add multiple values for halfOfYear to date range filter

--- web/assets/js/utils/date-range-picker.js
+++ web/assets/js/utils/date-range-picker.js
@@ -22,6 +22,34 @@ let ranges = {
     'Letztes Jahr': [now.subtract(1, 'year').startOf('year').toDate(), now.subtract(1, 'year').endOf('year').toDate()],
     'Vorletztes Jahr': [now.subtract(2, 'year').startOf('year').toDate(), now.subtract(2, 'year').endOf('year').toDate()],
 };
+for (let i = -1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        continue;
+    }
+    const quarter = now.subtract(i, 'quarter').quarter();
+    let halfOfYear;
+    switch (quarter) {
+        case 1:
+            halfOfYear = 1;
+            break;
+        case 2:
+            halfOfYear = 1;
+            break;
+        case 3:
+            halfOfYear = 2;
+            break;
+        case 4:
+            halfOfYear = 2;
+            break;
+        default:
+            halfOfYear = 1;
+    }
+
+    ranges[`${now.subtract(i, 'quarter').year()} ${halfOfYear}. Halbjahr`] = [
+        now.subtract(i + 1, 'quarter').startOf('quarter').toDate(),
+        now.subtract(i, 'quarter').endOf('quarter').toDate()
+    ];
+}
 for (let i = 1; i <= 4; i++) {
     ranges[`${now.subtract(i, 'quarter').year()} ${now.subtract(i, 'quarter').quarter()}. Quartal`] = [
         now.subtract(i, 'quarter').startOf('quarter').toDate(),
