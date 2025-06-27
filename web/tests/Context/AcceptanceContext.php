5633e344346d8c8afbbef3d10604baf4a38c7925
robertfausk <robertfreigang@gmx.de>
2025-06-27 09:20:25 +0200

    There is no need to click on default vSelect item
    because it has also an enter keypress

--- web/tests/Context/AcceptanceContext.php
+++ web/tests/Context/AcceptanceContext.php
@@ -508,7 +508,6 @@ final class AcceptanceContext extends MinkContext
         $isDivField = $isVCombobox || $isVTextarea || $isVTextField || $isVSelect;
         if ($isDivField) {
             $element->click();
-            dump('here' . ' ' . $value);
             $element->keyPress(\str_repeat(WebDriverKeys::BACKSPACE, 25));
             $element->keyPress($this->enrichText($value));
             if (!$isVTextarea && !$isVTextField) {
@@ -517,7 +516,7 @@ final class AcceptanceContext extends MinkContext
             if ($isVSelect) {
                 $element->keyPress(WebDriverKeys::ENTER);
             }
-            if (!$isVTextField || $isVSelect) {
+            if (!$isVTextField && !$isVSelect) {
                 $parent = $element->getParent();
                 Assert::true($parent->isVisible(), \sprintf('The element %s is not visible and so value %s can not be set.', $parent->getHtml(), $value));
                 $parent->click();
