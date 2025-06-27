10d239b88267b08a9c72e2384970dc33d4d4dc83
robertfausk <robertfreigang@gmx.de>
2025-06-27 23:27:27 +0200

    We need to wait 0,1 seconds before click on the vSelect item

--- web/tests/Context/AcceptanceContext.php
+++ web/tests/Context/AcceptanceContext.php
@@ -520,6 +520,7 @@ final class AcceptanceContext extends MinkContext
             if (!$isVTextField && !$isVSelect) {
                 $parent->click();
             } elseif ($isVSelect && $parent->isVisible()) {
+                \usleep(100000); // 0,1s
                 $parent->click();
             }
 
