fbbc7eae10da75564cf94bbce3f46a09b7321d6b
robertfausk <robertfreigang@gmx.de>
2026-04-29 23:01:45 +0200

    Replace @mdi/js star import with named tree-shakable imports
    The previous `import * as mdijs from '@mdi/js'` pulled all ~7000 Material Design Icons into the bundle, even though templates only use <mdicon name="..."/> for 11 of them. Vite/Rollup cannot tree- shake a star import that is then handed wholesale into a runtime plugin.

--- web/assets/js/app.ts
+++ web/assets/js/app.ts
@@ -12,7 +12,24 @@ import VueSilentbox from 'vue-silentbox';
 import 'vue-silentbox/dist/style.css';
 import VueClipboard from 'vue-clipboard2';
 import mdiVue from 'mdi-vue/v3';
-import * as mdijs from '@mdi/js';
+// Tree-shakable named imports statt `import * as mdijs from '@mdi/js'` -
+// der Star-Import zog ALLE ~7000 MDI-Icons in den Bundle, hier kommen nur
+// die rein, die in Templates per <mdicon name="..."/> verwendet werden.
+// Wenn ein neues Icon ergänzt wird: hier einkommentieren UND in mdiIcons
+// unten registrieren.
+import {
+    mdiAccount,
+    mdiAccountCircleOutline,
+    mdiAccountOff,
+    mdiAccountSupervisor,
+    mdiCheckCircleOutline,
+    mdiContentCopy,
+    mdiEmoticonHappyOutline,
+    mdiLockOutline,
+    mdiPlusCircleOutline,
+    mdiTagOff,
+    mdiWifiOff,
+} from '@mdi/js';
 import { VueDatePicker } from '@vuepic/vue-datepicker';
 
 import Swapp from './Swapp.vue';
@@ -30,7 +47,21 @@ registerErrorHandler(app);
 // Register global components & plugins
 app.component('VueDatePicker', VueDatePicker);
 app.use(pinia);
-app.use(mdiVue, { icons: mdijs });
+app.use(mdiVue, {
+    icons: {
+        mdiAccount,
+        mdiAccountCircleOutline,
+        mdiAccountOff,
+        mdiAccountSupervisor,
+        mdiCheckCircleOutline,
+        mdiContentCopy,
+        mdiEmoticonHappyOutline,
+        mdiLockOutline,
+        mdiPlusCircleOutline,
+        mdiTagOff,
+        mdiWifiOff,
+    },
+});
 app.use(vuetify);
 app.use(VueAxios, axios);
 app.use(router);
